adf2509eb3d003bb276e09ad5c0fb13745c55913
Gergo <tar.gergo21@gmail.com>
2023-03-09 16:25:33 +0100

    ``

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function init(){
     const LINKLIST =["kep1.jpg,kep2.jpg,kep3.jpg,kep4.jpg,kep5.jpg,kep6.jpg,kep7.jpg,kep8.jpg"]
     const ARTICLE = document.querySelectorAll("article")
     for (let index = 0; index < LINKLIST.length; index++) {
-        ARTICLE[0].innerHTML += ""
+        ARTICLE[0].innerHTML += ``
         
     }
 
