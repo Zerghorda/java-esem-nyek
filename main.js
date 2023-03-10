924f818ce337c5c555bdb614ee7ca8ac3181eb46
Gergo <tar.gergo21@gmail.com>
2023-03-10 11:03:51 +0100

    kicsiről nagy

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function preImg() {
 
 function nextImg() {
   const IMAGE = document.querySelectorAll("section div img");
-  image[0].src = LINKLIST[nagykep + 1];
+  IMAGE[0].src = LINKLIST[nagykep + 1];
   nagykep = nagykep + 1;
   if (nagykep == 7) {
     nagykep = 0;
